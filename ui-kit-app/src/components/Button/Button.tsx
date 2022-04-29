06d846d99f40febfa48e24df7e5415a816e50bb9
Vladislav <simwebkz@gmail.com>
2022-04-29 14:07:59 +0600

    link lib with porject

--- ui-kit-app/src/components/Button/Button.tsx
+++ ui-kit-app/src/components/Button/Button.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from "react";
+import "./button.css";
 
 export interface ButtonProps {
   color: string;
-  isBig: boolean;
+  isBig?: boolean;
   children?: React.ReactNode;
 }
 
 const Button: FC<ButtonProps> = ({ children, color, isBig, ...props }) => {
+  const rootClasses = ["rh-button"];
+  if (isBig) rootClasses.push("rh-button_big");
   return (
-    <button style={{ color }} {...props}>
+    <button className={rootClasses.join(" ")} style={{ color }} {...props}>
       {children}
     </button>
   );
